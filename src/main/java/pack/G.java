9c215a9aec11c441cdcdf5b48f6ceeea623dc0fb
shabeerAhmed9840 <shabeerahmed1996@outlook.com>
2020-10-10 16:13:40 +0530

    changes is good

--- src/main/java/pack/G.java
+++ src/main/java/pack/G.java
@@ -3,5 +3,8 @@ package pack;
 public class G {
     public static void main(String[] args) {
         System.out.println("Great");
+        //Wont it be great
+        //if its goood then its good
+        //good time is coming
     }
 }
